d940797a80f710f38fd8bc84f85235c799355f68
Aishah999 <aishaah@code.edu.az>
2024-10-20 12:49:41 +0400

    second commit

---
+++ Array Task1/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace Array_Task1;
+class Program
+{
+    static void Main(string[] args)
+
+    {
+        #region
+        //var product1 = new { Id = 1, Name = "Powder", Price = 30, StockCount = 50 };
+        //var product2 = new { Id = 2, Name = "Foundation", Price = 56, StockCount = 100 };
+        //var product3 = new { Id = 3, Name = "Blush", Price = 23, StockCount = 30 };
+        //var product4 = new { Id = 5, Name = "Primer", Price = 16, StockCount = 20 };
+
+        //var products = new[] { product1, product2, product3, product4 };
+        //int sum = 0;
+        //int avg = 0;
+        //int count = 0;
+
+        //for(int i=0;i<products.Length;i++)
+        //{
+            //if (products[i].Id%2==1)
+            //{
+                //count++;
+                //sum += products[i].Price;
+
+            //}
+        //}
+        //avg = sum / count;
+        //Console.WriteLine(avg);
+
+        #endregion
+
+
+    }
+}
+
